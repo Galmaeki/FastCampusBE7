054266abb2bf5bae439b5506bd00d71136a61843
Galmaeki <kjh09800@gmail.com>
2023-12-04 19:06:48 +0900

    입력 주석 추가

--- src/javaDay5/MovieProcessor.java
+++ src/javaDay5/MovieProcessor.java
@@ -14,6 +14,12 @@ public class MovieProcessor {
             str = scan.nextLine();
             if(str.equals("I")){
                 while(true){
+                    /*
+영화1: '기생충', 주연: '김기택', 러닝타임: 132, 평점: 8.6, 장르: 1
+영화2: '올드보이', 주인공: '오대수', 러닝타임: 120, 평점: 8.4, 장르: 2
+영화3: '괴물', 주인공: '박강두', 러닝타임: 119, 평점: 7.1, 장르: 3
+                     한줄씩 그대로 입력
+                    * */
                     System.out.println("영화를 입력해 주세요");
                     String a = scan.nextLine();
                     if(a.equals("-1")) break;
